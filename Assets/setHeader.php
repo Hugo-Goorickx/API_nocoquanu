c55e250c46b431fb71a41d220f3d6edeb1459ea1
Hugo Goorickx <hugo@goorickx.be>
2023-03-25 14:52:44 +0100

    Ajout de l'API basique

--- Assets/setHeader.php
+++ Assets/setHeader.php
@@ -6,9 +6,9 @@ if (isset($_SERVER['HTTP_ORIGIN']))
     header('Access-Control-Max-Age: 1000');
 }
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) 
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         header("Access-Control-Allow-Methods: POST, GET, PATCH, OPTIONS, PUT, DELETE");
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+        header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Key");
     exit(0);
 }
