4d7729f2b842d2ced0839a7842ac00ba402a4ddd
Hugo Goorickx <hugo@goorickx.be>
2023-03-25 15:09:57 +0100

    test

--- Assets/request.php
+++ Assets/request.php
@@ -7,7 +7,7 @@ function createRequest($request)
     try                 
     { 
 //        $bdd = new \PDO('mysql:host=127.0.0.1;dbname=bouh', 'root', '');
-        $bdd = new \PDO('mysql:host=hugoorickx.be:3306;dbname=u716273791_nocoquanu;charset=utf8', 'u716273791_user', 'K3orOZDj38b^');
+        $bdd = new \PDO('mysql:host=sql929.main-hosting.eu;dbname=u716273791_nocoquanu;charset=utf8', 'u716273791_user', 'K3orOZDj38b^');
 
         $ps= $bdd->prepare($request);
         $ps->execute();
